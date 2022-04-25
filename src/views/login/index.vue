c8c0969880f0a69876d51432d66fba3db3d3d44f
Longafter <zhangpingzhao_cs@163.com>
2022-04-25 22:12:53 +0800

    创建login.vue

---
+++ src/views/login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-container">
+    <el-form :model="form">
+      <el-form-item>
+        <el-input v-model="form.name" />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const form = ref({
+  name: ''
+})
+</script>
+
+<style scoped lang='sass'></style>
