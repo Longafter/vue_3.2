725604db4690fd9a683dc13a80add407182500da
Longafter <zhangpingzhao_cs@163.com>
2022-04-25 21:29:58 +0800

    按需导入element-plus

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
+module.exports = {
+  configureWebpack: config => {
+    // 按需导入 element-plus
+    config.plugins.push(AutoImport({
+      resolvers: [ElementPlusResolver()]
+    }))
+    config.plugins.push(Components({
+      resolvers: [ElementPlusResolver()]
+    }))
+  }
+}
