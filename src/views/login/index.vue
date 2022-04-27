823f5d73e9e39a2f94794a84134ddbd61e1ad059
longafter <zhangpingzhao_cs@163.com>
2022-04-27 15:46:27 +0800

    login页使用i18n

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,7 +2,7 @@
   <div class="login-container">
     <el-form :model="form" :rules="rules" ref="formRef" class="login-form">
       <div class="title-container">
-        <h3 class="title">用户登录</h3>
+        <h3 class="title">{{ $t('msg.title') }}</h3>
       </div>
       <el-form-item prop="username">
         <svg-icon icon="user" class="svg-container"></svg-icon>
